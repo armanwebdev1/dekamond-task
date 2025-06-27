2f97526498c55dd208b7f067bbeac26001dbb3d6
آرمان احمدپور <armantranslates@gmail.com>
2025-06-27 22:34:55 +0430

    Added responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
